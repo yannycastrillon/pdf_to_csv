9e88c9b03a4e76520f9797eedadabf0903bebea7
Yanny Castrillon <yannyandres117@gmail.com>
2017-10-24 11:43:51 -0700

    Build functionality to parse an unformatted .csv file from a PDF to a formatted .csv file. Incorporate TDD (Test Driven Development) on the main functionalites of the application to assure the correct process.

---
+++ pdf_to_csv.rb
@@ -0,0 +1,126 @@
+require 'csv'
+
+class PDFToCSV
+  attr_accessor :my_data, :arr_headers_line_csv, :arr_age_rate_line_csv
+  HEADERS_COLUMNS = [ 'age', 'price', 'region', 'plan_id', 'issuer_name', 'plan_name']
+
+  def initialize(file_name)
+    @@file = File.open(file_name)
+    @arr_headers_line_csv = []
+    @arr_age_rate_line_csv = []
+    @my_data = Hash.new
+
+    i = 0
+    while (line = @@file.gets)
+      # validates when it is the fouth line to separate headers from age_rates.
+      i <= 4 ? @arr_headers_line_csv << line : @arr_age_rate_line_csv << line
+      # controls when the numbers "age_rate" begins on the file.
+      i += 1
+    end
+  end
+
+  # 1) maps each string line of the array_info.
+  # 2) Each section gets splitted by ',' into an array.
+  # 3) Pops out last element which is '/\r\n/'.
+  # 4) Filter elements by ''.
+  def format_array(unformated_array)
+    begin
+      unformated_array.map do |section|
+        arr = section.split(',')
+        arr.pop
+        arr.select do |ele|
+          ele != ''
+        end
+      end
+    rescue Exception => e
+      raise "Invalid Format"
+    end
+  end
+
+  # Assigns static data to instance variable "@my_data" hash
+  def add_description_data(hash)
+    hash.keys.each {|key| @my_data[key] = hash[key]}
+
+    puts "----------- AFTER DESCRIPTION  -----------"
+    puts @my_data
+    puts "----------------------"
+  end
+
+  # Class method in charge of retrieving the portion of data from each headers array-string.
+  def self.retrieve_value_with_regexp(formated_headers)
+    hash = Hash.new
+    # "Rate" Regexp to extract rate.
+    hash[:rate] = formated_headers[1][0][/\d+/].to_i
+    # "Plan_id" Regexp to extract plan id.
+    hash[:plan_id] = formated_headers[2][0][/\d+/].to_i
+    # "Plan_name" Regexp to extract plan name.
+    hash[:plan_name] = formated_headers[2][0].scan(/\w+/)[4...formated_headers[2][0].scan(/\w+/).length-2].join(' ')
+    hash[:issuer_name] = "Aetna"
+    puts "----------- REGEX VALUES -----------"
+    puts hash
+    puts "----------------------"
+
+    hash
+  end
+
+  # 1) flatten the array to ungroup them from columns of 6.
+  # 2) group them in consecutives tuples.
+  # 3) Turn everything into a new hash.
+  # 4) Merge the two hash together to have the final data hash.
+  def formatted_age_rate_data(array)
+    array.flatten.each_slice(2).to_h
+  end
+
+
+  # Constructs and prepares array to be able to export to .csv file
+  def build_final_data_arr
+    symbol_hash = @my_data.select { |key, value| value if key.is_a?(Symbol) }
+    final_data_arr = @my_data.to_a.map { |row_array| row_array +  symbol_hash.values }
+    final_data_arr.slice(0..final_data_arr.length - 5)
+  end
+
+  # Export "final_data_arr" array into formatted .csv file.
+  def export_data_arr_to_csv(data_arr)
+    s = CSV.generate do |csv|
+      csv << HEADERS_COLUMNS
+      data_arr.each{ |row| csv << row }
+    end
+    File.write('formatted_data.csv', s)
+    @@file.close
+  end
+end
+
+# Object of the class PDFToCSV and initializer
+# - "arr_headers_line_csv" and "arr_age_rate_line_csv"
+obj = PDFToCSV.new("./tabula-aeta_sample_p1.csv")
+
+arr_headers_line_csv = obj.arr_headers_line_csv
+formated_headers = obj.format_array(arr_headers_line_csv)
+
+puts "---------------formated_headers--------------"
+p formated_headers
+puts "-----------------------------"
+
+obj.add_description_data(PDFToCSV.retrieve_value_with_regexp(formated_headers));
+
+arr_age_rate_line_csv = obj.arr_age_rate_line_csv
+
+formatted_age_rate = obj.format_array(arr_age_rate_line_csv)
+puts "--------------- formatted_age_rate--------------"
+p formatted_age_rate
+puts "-----------------------------"
+
+age_rate_hash = obj.formatted_age_rate_data(formatted_age_rate)
+
+puts "--------------- age_rate_hash --------------"
+p age_rate_hash
+puts "-----------------------------"
+
+obj.my_data = age_rate_hash.merge(obj.my_data)
+
+puts "----------- final_data_arr ------------"
+p obj.build_final_data_arr
+puts "----------- final_data_arr ------------"
+
+# take out
+obj.export_data_arr_to_csv(obj.build_final_data_arr)
---
+++ spec/pdf_to_csv_test.rb
@@ -0,0 +1,124 @@
+require 'spec_helper'
+require './pdf_to_csv'
+
+describe 'Test process to format an unformated CSV file into a formatted CSV file' do
+  before :each do
+    @file_name = './tabula-aeta_sample_p1.csv'
+    @pdf_to_csv = PDFToCSV.new(@file_name)
+  end
+
+  describe PDFToCSV, '#initialize' do
+    context 'when a new intance of PDFToCSV' do
+      it 'should initialize @arr_headers_line_csv, @arr_age_rate_line_csv and @my_data' do
+        expect(@pdf_to_csv).to be_an_instance_of(PDFToCSV)
+        expect(@pdf_to_csv.arr_headers_line_csv).to be_instance_of(Array)
+        expect(@pdf_to_csv.arr_age_rate_line_csv).to be_instance_of(Array)
+        expect(@pdf_to_csv.my_data).to be_instance_of(Hash)
+      end
+
+      it 'should open file' do
+        expect(@file_name).to eq('./tabula-aeta_sample_p1.csv')
+        allow(File).to receive(:open).with(@file_name)
+      end
+
+      it 'should add line to either @arr_headers_line_csv or @arr_age_rate_line_csv' do
+        expect(@pdf_to_csv.arr_headers_line_csv.length).to be > 0
+        expect(@pdf_to_csv.arr_age_rate_line_csv.length).to be > 0
+      end
+    end
+  end
+
+  describe PDFToCSV, '#format_array' do
+    context 'when is call by the class instance' do
+      it 'should respond to format_array with 1 argument ' do
+        expect(@pdf_to_csv).to respond_to(:format_array).with(1).argument
+      end
+    end
+    context 'when Array is passed as argument' do
+      let(:array) { ["CALIFORNIA,\r\n", "Rating Area:CARA01 *,\r\n", "Plan ID:14033847Plan,\r\n", "Age,Rate,\r\n"] }
+
+      it 'should format the array' do
+        expect(@pdf_to_csv.format_array(array)).to eql([["CALIFORNIA"], ["Rating Area:CARA01 *"], ["Plan ID:14033847Plan"], ["Age", "Rate"]])
+      end
+    end
+    context 'when something different than an Array is passed as argument' do
+      let(:string) { 'something' }
+
+      it 'raise an error' do
+        expect { @pdf_to_csv.format_array(string) }.to raise_error("Invalid Format")
+      end
+    end
+  end
+
+  describe PDFToCSV, '#add_description_data' do
+    context 'when is call by the class instance ' do
+      it 'should receive hash argument' do
+        expect(@pdf_to_csv).to respond_to(:add_description_data).with(1).argument
+      end
+
+      it 'should populate @my_data hash' do
+        @pdf_to_csv.add_description_data(rate:1,plan_id:123456,plan_name:"default", issuer_name:"aetna")
+        expect(@pdf_to_csv.my_data.length).to be > 0
+      end
+    end
+  end
+
+  describe PDFToCSV, '#formatted_age_rate_data' do
+    context 'when is call by the class instance ' do
+      let(:array) { [["CALIFORNIA"], ["Rating:CARA01 *"], ["Plan ID:14033847Plan Name:CA Silver"], ["Age"]] }
+
+      it 'should respond to formatted_age_rate_data with 1 argument' do
+        expect(@pdf_to_csv).to respond_to(:formatted_age_rate_data).with(1).argument
+      end
+      it 'should return a hash' do
+        expect(@pdf_to_csv.formatted_age_rate_data(array)).to be_instance_of(Hash)
+      end
+    end
+  end
+
+  describe PDFToCSV, '#build_final_data_arr ' do
+    context 'when is call by the class instance ' do
+      let(:symbol_hash) { @pdf_to_csv.my_data.select { |key, value| value if key.is_a?(Symbol) } }
+      let(:age_rate_hash) { { "0-20"=>"421.26", "35"=>"810.67", "50"=>"1184.82" } }
+      let(:final_data_arr) { @pdf_to_csv.my_data = age_rate_hash.merge(@pdf_to_csv.my_data) }
+
+      it 'should select values from @my_data which keys are symbols ' do
+        expect(final_data_arr.length).to be > 0
+      end
+      it 'should return an Array with concatenated values from @my_data array' do
+        expect(@pdf_to_csv.build_final_data_arr).to be_instance_of(Array)
+      end
+    end
+  end
+
+  describe PDFToCSV, '#export_data_arr_to_csv ' do
+    before do
+      @data_arr = [["0-20", "421.26", 1, 14033847, "CA Silver Indemnity 1500 80", "Aetna"],
+                   ["35", "810.67", 1, 14033847, "CA Silver Indemnity 1500 80", "Aetna"],
+                   ["50", "1184.82", 1, 14033847, "CA Silver Indemnity 1500 80", "Aetna"],
+                   ["44", "926.76", 1, 14033847, "CA Silver Indemnity 1500 80", "Aetna"],
+                  ]
+      HEADERS_COLUMNS = ['age', 'price', 'region', 'plan_id', 'issuer_name', 'plan_name']
+      @s = CSV.generate do |csv|
+        csv << HEADERS_COLUMNS
+        @data_arr.each{ |row| csv << row }
+      end
+    end
+    context 'when is call by the class instance ' do
+      let(:file_name) { 'formatted_data.csv' }
+      let(:file) { File.open('./tabula-aeta_sample_p1.csv')}
+      it 'should receive only one argumen ' do
+        expect(@pdf_to_csv).to respond_to(:export_data_arr_to_csv).with(1).argument
+      end
+      it 'should generate a csv' do
+        expect(@s).to be_instance_of(String)
+      end
+      it 'should open a File to write the csv' do
+          allow(File).to receive(:write).with(file_name)
+      end
+      it 'should close the @@file connection' do
+          expect(file).to respond_to(:close)
+      end
+    end
+  end
+end
